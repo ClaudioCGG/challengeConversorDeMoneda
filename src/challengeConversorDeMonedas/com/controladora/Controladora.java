c3bbebcc31fde06158ac5448dbd52af53087c5d8
Claudio Guirado <claudioguirado@hotmail.com>
2024-04-29 18:13:06 -0300

    Agrega proyecto Java Inicial

---
+++ src/challengeConversorDeMonedas/com/controladora/Controladora.java
@@ -0,0 +1,17 @@
+package challengeConversorDeMonedas.com.controladora;
+
+import java.util.Currency;
+
+public class Controladora {
+
+    // Método para actualizar las tasas de cambio
+    public void actualizadorTasasDeCambio() {
+        // Implementa la lógica para actualizar las tasas de cambio desde una fuente externa
+    }
+
+    // Método para obtener la tasa de cambio entre dos divisas específicas
+    public double getTasaDeCambio(Currency fromCurrency, Currency toCurrency) {
+        // Implementa la lógica para obtener la tasa de cambio entre dos divisas
+        return 0;
+    }
+}
---
+++ src/modelo/Divisa.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Divisa {
+    private String nombre;
+    private String codigo;
+    private double tasaDeCambio;
+
+}
