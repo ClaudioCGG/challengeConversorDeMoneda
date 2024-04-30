613ee1d71e451e1e39a6750dbba8a002f733be81
Claudio Guirado <claudioguirado@hotmail.com>
2024-04-30 12:14:36 -0300

    Proyecto avanzado falta readme segun la nota alura

--- src/modelo/Divisa.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public class Divisa {
-    private String nombre;
-    private String codigo;
-    private double tasaDeCambio;
-
-}
---
+++ src/modelo/TasasDeCambioResponse.java
@@ -0,0 +1,24 @@
+package challengeConversorDeMonedas.com.controladora;
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+public class TasasDeCambioResponse {
+    private String resultado;
+
+    // Otros campos del JSON que no necesitamos para esta implementación
+    private Map<String, Double> tasasDeCambio;
+
+
+    // Método para obtener las tasas de conversión
+    public Map<String, Double> getConversionRates() {
+        return tasasDeCambio;
+    }
+
+    // Método para parsear el JSON
+    public static TasasDeCambioResponse fromJson(String json) {
+        Gson gson = new Gson();
+
+        return gson.fromJson(json, TasasDeCambioResponse.class);
+    }
+}
